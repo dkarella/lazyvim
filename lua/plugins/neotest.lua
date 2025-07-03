37f4516d6f91838f7aabe947dbbd4c5057c79de0
Danny Arellano <arellanodanielkeith@gmail.com>
2025-07-03 10:57:23 -0700

    updates

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,39 +1,37 @@
 return {
-  { "nvim-neotest/nvim-nio" },
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "fredrikaverpil/neotest-golang",
-      "mrcjkb/rustaceanvim",
+  "nvim-neotest/neotest",
+  dependencies = {
+    "fredrikaverpil/neotest-golang",
+    "mrcjkb/rustaceanvim",
+    "nvim-neotest/nvim-nio",
+  },
+  opts = {
+    -- See all config options with :h neotest.Config
+    discovery = {
+      -- Drastically improve performance in ginormous projects by
+      -- only AST-parsing the currently opened buffer.
+      enabled = false,
+      -- Number of workers to parse files concurrently.
+      -- A value of 0 automatically assigns number based on CPU.
+      -- Set to 1 if experiencing lag.
+      concurrent = 1,
     },
-    opts = {
-      -- See all config options with :h neotest.Config
-      discovery = {
-        -- Drastically improve performance in ginormous projects by
-        -- only AST-parsing the currently opened buffer.
-        enabled = false,
-        -- Number of workers to parse files concurrently.
-        -- A value of 0 automatically assigns number based on CPU.
-        -- Set to 1 if experiencing lag.
-        concurrent = 1,
-      },
-      running = {
-        -- Run tests concurrently when an adapter provides multiple commands to run.
-        concurrent = true,
-      },
-      summary = {
-        -- Enable/disable animation of icons.
-        animated = false,
-      },
+    running = {
+      -- Run tests concurrently when an adapter provides multiple commands to run.
+      concurrent = true,
+    },
+    summary = {
+      -- Enable/disable animation of icons.
+      animated = false,
+    },
 
-      adapters = {
-        ["neotest-golang"] = {
-          go_test_args = { "-v", "-race", "-count=1", "-timeout=30s" },
-          testify_enabled = true,
-          dap_go_enabled = true,
-        },
-        ["rustaceanvim.neotest"] = {},
+    adapters = {
+      ["neotest-golang"] = {
+        go_test_args = { "-v", "-race", "-count=1", "-timeout=30s" },
+        testify_enabled = true,
+        dap_go_enabled = true,
       },
+      ["rustaceanvim.neotest"] = {},
     },
   },
 }
