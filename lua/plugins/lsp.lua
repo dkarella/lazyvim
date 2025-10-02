219a531e5df7539d7d270205ecc530626f7dce6d
Danny Arellano <danny@siftstack.com>
2025-10-02 12:10:25 -0700

    fix rust-analyzer settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,15 +4,15 @@ return {
     opts = {
       inlay_hints = { enabled = false },
 
-      ["rust-analyzer"] = {
-        settings = {
-          ["rust-analyzer"] = {
-            procMacro = { enable = true },
+      servers = {
+        ["rust-analyzer"] = {
+          settings = {
+            ["rust-analyzer"] = {
+              procMacro = { enable = true },
+            },
           },
         },
       },
-
-      servers = {},
     },
   },
 }
