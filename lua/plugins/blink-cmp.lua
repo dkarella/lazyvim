13a431e226940b812debc24798664cee46799fa1
Danny Arellano <danny@siftstack.com>
2026-02-20 10:46:30 -0800

    fix tab completion

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -5,6 +5,18 @@ return {
     keymap = {
       -- https://cmp.saghen.dev/configuration/keymap.html
       preset = "super-tab",
+      ["<Tab>"] = {
+        function()
+          local suggestion = require("copilot.suggestion")
+          if suggestion.is_visible() then
+            suggestion.accept()
+            return true
+          end
+        end,
+        "snippet_forward",
+        "select_next",
+        "fallback",
+      },
       ["<C-k>"] = { "select_prev", "fallback" },
       ["<C-j>"] = { "select_next", "fallback" },
       ["<CR>"] = { "accept", "fallback" },
