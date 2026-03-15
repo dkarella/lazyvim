f44bff0d97f4633735e5c4f3657e0f2b421536a0
Danny Arellano <arellanodanielkeith@gmail.com>
2026-03-14 23:39:40 -0700

    hidden files

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,5 +2,18 @@ return {
   "snacks.nvim",
   opts = {
     scroll = { enabled = false },
+    explorer = {
+      hidden = true,
+    },
+    picker = {
+      sources = {
+        files = {
+          hidden = true,
+        },
+        grep = {
+          hidden = true,
+        },
+      },
+    },
   },
 }
