5d19a6457c7974b28ab5249361d2d1bb6ecf4933
Danny Arellano <danny@siftstack.com>
2026-04-20 10:58:40 -0700

    close window/buffer

--- lua/nvim/keymaps.lua
+++ lua/nvim/keymaps.lua
@@ -3,3 +3,5 @@ local map = vim.keymap.set
 
 map('n', '|', '<C-W>s', { desc = 'Split Window Below', remap = true })
 map('n', '\\', '<C-W>v', { desc = 'Split Window Right', remap = true })
+map('n', '<leader>wd', '<C-W>c', { desc = 'Close Window', remap = true })
+map('n', '<leader>bd', function() Snacks.bufdelete() end, { desc = 'Delete Buffer' })
