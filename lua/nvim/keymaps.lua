c0c793b44412b0b0709b1637ab60710c6fe7345b
Danny Arellano <arellanodanielkeith@gmail.com>
2026-04-25 22:39:06 -0700

    updates

--- lua/nvim/keymaps.lua
+++ lua/nvim/keymaps.lua
@@ -1,13 +1,15 @@
 -- Standalone-only keymaps (VS Code handles splits via its own commands)
 local map = vim.keymap.set
 
-map('n', '|', '<C-W>s', { desc = 'Split Window Below', remap = true })
-map('n', '\\', '<C-W>v', { desc = 'Split Window Right', remap = true })
-map('n', '<leader>wd', '<C-W>c', { desc = 'Close Window', remap = true })
-map('n', '<leader>bd', function() Snacks.bufdelete() end, { desc = 'Delete Buffer' })
+map("n", "|", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "\\", "<C-W>v", { desc = "Split Window Right", remap = true })
+map("n", "<leader>wd", "<C-W>c", { desc = "Close Window", remap = true })
+map("n", "<leader>bd", function()
+	Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
 
 -- Terminal mode: ctrl-hjkl moves out of the terminal window
-map('t', '<C-h>', [[<C-\><C-n><C-w>h]], { desc = 'Focus Left Window' })
-map('t', '<C-j>', [[<C-\><C-n><C-w>j]], { desc = 'Focus Below Window' })
-map('t', '<C-k>', [[<C-\><C-n><C-w>k]], { desc = 'Focus Above Window' })
-map('t', '<C-l>', [[<C-\><C-n><C-w>l]], { desc = 'Focus Right Window' })
+map("t", "<C-h>", [[<C-\><C-n><C-w>h]], { desc = "Focus Left Window" })
+map("t", "<C-j>", [[<C-\><C-n><C-w>j]], { desc = "Focus Below Window" })
+map("t", "<C-k>", [[<C-\><C-n><C-w>k]], { desc = "Focus Above Window" })
+map("t", "<C-l>", [[<C-\><C-n><C-w>l]], { desc = "Focus Right Window" })
--- lua/nvim/plugins/snacks.lua
+++ lua/nvim/plugins/snacks.lua
@@ -1,32 +1,83 @@
 -- Snacks: terminal (LazyDocker), file explorer, fuzzy picker
 return {
-  'folke/snacks.nvim',
-  priority = 1000,
-  lazy = false,
-  opts = {
-    terminal = {
-      win = {
-        position = 'right',
-        width = 0.5,
-        winbar = false,
-      },
-    },
-    explorer = { show_hidden = true },
-    picker = {
-      sources = {
-        files = { hidden = true },
-        grep  = { hidden = true },
-      },
-    },
-    scroll = { enabled = false },
-  },
-  keys = {
-    { '<leader>ld',     function() Snacks.terminal('lazydocker') end, desc = 'LazyDocker' },
-    { '<leader>e',      function() Snacks.explorer() end,             desc = 'File Explorer' },
-    { '<leader><leader>', function() Snacks.picker.files() end,        desc = 'Find Files' },
-    { '<leader>/',      function() Snacks.picker.grep() end,           desc = 'Grep' },
-    { '<leader>gb',     function() Snacks.git.blame_line() end,        desc = 'Git Blame Line' },
-    { '<leader>gB',     function() Snacks.gitbrowse() end,             desc = 'Git Browse (open)', mode = { 'n', 'v' } },
-    { '<leader>gg',     function() Snacks.lazygit({ win = { position = 'float', width = 0.9, height = 0.9 } }) end, desc = 'LazyGit' },
-  },
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		terminal = {
+			win = {
+				position = "bottom",
+				width = 0.5,
+				winbar = false,
+			},
+		},
+		explorer = { show_hidden = true },
+		picker = {
+			sources = {
+				files = { hidden = true },
+				grep = { hidden = true },
+			},
+		},
+		scroll = { enabled = false },
+	},
+	keys = {
+		{
+			"<C-/>",
+			function()
+				Snacks.terminal.toggle()
+			end,
+			desc = "Toggle Terminal",
+			mode = { "n", "t" },
+		},
+		{
+			"<leader>ld",
+			function()
+				Snacks.terminal("lazydocker")
+			end,
+			desc = "LazyDocker",
+		},
+		{
+			"<leader>e",
+			function()
+				Snacks.explorer()
+			end,
+			desc = "File Explorer",
+		},
+		{
+			"<leader><leader>",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader>/",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep",
+		},
+		{
+			"<leader>gb",
+			function()
+				Snacks.git.blame_line()
+			end,
+			desc = "Git Blame Line",
+		},
+		{
+			"<leader>gB",
+			function()
+				Snacks.gitbrowse()
+			end,
+			desc = "Git Browse (open)",
+			mode = { "n", "v" },
+		},
+		{
+			"<leader>gg",
+			function()
+				Snacks.lazygit({ win = { position = "float", width = 0.9, height = 0.9 } })
+			end,
+			desc = "LazyGit",
+		},
+	},
 }
