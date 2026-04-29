7968cc884ccdf29dee731d4dc33823760592a2d9
Danny Arellano <danny@siftstack.com>
2026-04-29 14:30:01 -0700

    neotest update

--- lua/nvim/plugins/neotest.lua
+++ lua/nvim/plugins/neotest.lua
@@ -15,12 +15,15 @@ return {
           args = { "-tags=unit,integration" },
         }),
       },
+      discovery = {
+        enabled = false,
+      },
     })
   end,
   keys = {
-    { "<leader>tr", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Test run file" },
-    { "<leader>tt", function() require("neotest").run.run() end,                  desc = "Test nearest" },
-    { "<leader>ts", function() require("neotest").summary.toggle() end,           desc = "Test summary" },
-    { "<leader>to", function() require("neotest").output.open({ enter = true }) end, desc = "Test output" },
+    { "<leader>tr", function() require("neotest").run.run() end,                          desc = "Test run nearest" },
+    { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end,        desc = "Test run file" },
+    { "<leader>ts", function() require("neotest").summary.toggle() end,                   desc = "Test summary" },
+    { "<leader>to", function() require("neotest").output.open({ enter = true, last_run = false }) end, desc = "Test output (cursor)" },
   },
 }
