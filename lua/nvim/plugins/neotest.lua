68cd5495d918ce5f2a7ca2d5c87d63b7e7e584fe
Danny Arellano <danny@siftstack.com>
2026-07-13 13:41:45 -0700

    neotest update

--- lua/nvim/plugins/neotest.lua
+++ lua/nvim/plugins/neotest.lua
@@ -1,29 +1,56 @@
 return {
-  "nvim-neotest/neotest",
-  dependencies = {
-    "nvim-neotest/nvim-nio",
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "rouge8/neotest-rust",
-    "nvim-neotest/neotest-go",
-  },
-  config = function()
-    require("neotest").setup({
-      adapters = {
-        require("neotest-rust"),
-        require("neotest-go")({
-          args = { "-tags=unit,integration" },
-        }),
-      },
-      discovery = {
-        enabled = false,
-      },
-    })
-  end,
-  keys = {
-    { "<leader>tr", function() require("neotest").run.run() end,                          desc = "Test run nearest" },
-    { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end,        desc = "Test run file" },
-    { "<leader>ts", function() require("neotest").summary.toggle() end,                   desc = "Test summary" },
-    { "<leader>to", function() require("neotest").output.open({ enter = true, last_run = false }) end, desc = "Test output (cursor)" },
-  },
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"rouge8/neotest-rust",
+		"fredrikaverpil/neotest-golang",
+	},
+	config = function()
+		require("neotest").setup({
+			adapters = {
+				require("neotest-rust"),
+				require("neotest-golang")({
+					experimental = {
+						testify_suites = true,
+					},
+					go_test_args = { "-v", "-race", "-count=1", "-tags=unit,integration" },
+				}),
+			},
+			discovery = {
+				enabled = false,
+			},
+		})
+	end,
+	keys = {
+		{
+			"<leader>tr",
+			function()
+				require("neotest").run.run()
+			end,
+			desc = "Test run nearest",
+		},
+		{
+			"<leader>tt",
+			function()
+				require("neotest").run.run(vim.fn.expand("%"))
+			end,
+			desc = "Test run file",
+		},
+		{
+			"<leader>ts",
+			function()
+				require("neotest").summary.toggle()
+			end,
+			desc = "Test summary",
+		},
+		{
+			"<leader>to",
+			function()
+				require("neotest").output.open({ enter = true, last_run = false })
+			end,
+			desc = "Test output (cursor)",
+		},
+	},
 }
